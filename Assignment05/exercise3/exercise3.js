98f6b1ab3b618d3efaa072978c9b0b603c084dbb
gyus-e <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2025-03-25 22:34:02 +0100

    exercises 2-3 done

--- Assignment05/exercise3/exercise3.js
+++ Assignment05/exercise3/exercise3.js
@@ -8,13 +8,19 @@ function createMultiStepFilterer(array){
 
     function filterer(filterCriterion){
         if (typeof filterCriterion === 'function') {
-            ret = ret.filter(filterCriterion);
+            let idx = 0;
+            for (let elem of ret) {
+                if (filterCriterion(elem)) {
+                    ret[idx] = elem;
+                    idx++;
+                }
+            }
+            ret.length = idx;
         }
         return ret;
     }
     return filterer;
 }
-
 
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; 
 let filterer = createMultiStepFilterer(arr); 
